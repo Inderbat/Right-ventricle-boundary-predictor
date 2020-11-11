90b3a659d3a0746f337aecb9781d4f5580ecd8b6
Inderbat <42816355+Inderbat@users.noreply.github.com>
2020-11-11 20:39:56 +0530

    Update main.py
    Adding evaluation code to the main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 '''
 	Joint Recognition of inner and outer boundary
 '''
-
-import argparse
 from train_utils import *
 
 if __name__ == '__main__':
 
-	# parsing the command to format the dataset
-    parser = argparse.ArgumentParser(description='Input among the following: train, test1, test2')
+	# saving all the models
+	save_models('loss_type')
+	save_models('optimizer_type')
+	save_models('learning_rate_scheduler')
+
+	# creating the data loader for testing data
+	data_loader_test_1 = load_data('test_set_1', dataset_split=None)
+	data_loader_test_2 = load_data('test_set_2', dataset_split=None)
+
+	# evaluating all the models
+	model_evaluate(data_loader_test_1)
+	model_evaluate(data_loader_test_2)
